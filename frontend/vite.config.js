c6c557b2952bf54be5159001eea28bd42ad7e9ca
omercalc <omer.c.alici@outlook.com>
2024-09-22 16:13:39 +0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Eğer manuel parçalara bölme gerekiyorsa burada ayarlayın
+      },
+    },
+  },
+  server: {
+    port: 5173, 
+    open: true, 
+  },
+});
